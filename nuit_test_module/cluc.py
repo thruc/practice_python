a04a4c639ca7389a6564a3d0cb3a697443300cb7
thruc <horiuchi.github@gmail.com>
2020-12-27 03:34:57 +0900

    ユニットテスト　開始

---
+++ nuit_test_module/cluc.py
@@ -0,0 +1,9 @@
+class Culc(object):
+    """Add and Doubel
+    """
+    def add_and_doubel(self, x, y):
+        """Add and Doubel
+        """
+        result = x + y
+        result *= 2
+        return result 
