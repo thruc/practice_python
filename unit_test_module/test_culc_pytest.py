6f9a0fd68eecbc7e0ba1eeceb23908b78da59806
Tomo-Hroiuchi <horiuchi.github@gmail.com>
2021-01-05 04:20:19 +0900

    例外　スキップ

--- unit_test_module/test_culc_pytest.py
+++ unit_test_module/test_culc_pytest.py
@@ -1,7 +1,36 @@
 from unit_test_module import cluc
+import pytest
 
-def test_add_num_and_double():
-    cal = cluc.Culc()
-    assert cal.add_and_doubel(1, 1) == 4
+is_release = True
+class TestCal:
+    @classmethod
+    def setup_class(cls):
+        print('start')
+
+    @classmethod
+    def teardown_class(cls):
+        print('end')
+
+    
+    def setup_method(self, method):
+        # テストそれぞれ開始時に呼ばれる
+        print('setup')
+        self.cal = cluc.Culc()
+
+    def teardown_method(self, method):
+        # テストそれぞれ終了時時に呼ばれる
+        print('clean up')
+        del self.cal
+
+    def test_add_num_and_double(self):
+        assert self.cal.add_and_doubel(1, 1) == 4
+
+    def test_add_num_and_double_raise(self):
+        # 例外テスト　pytest.raisesを使用
+        with pytest.raises(ValueError):
+            (self.cal.add_and_doubel("1", "1"))
 
 
+    @pytest.mark.skipif(is_release==True, reason="is_release")
+    def test_add_num_and_double_skip_if(self):
+        assert self.cal.add_and_doubel(1, 1) == 4
