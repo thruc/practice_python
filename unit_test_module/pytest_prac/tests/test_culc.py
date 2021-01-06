9bd91ca7bbb284a83e239512739fc4734ef8045c
thruc <horiuchi.github@gmail.com>
2021-01-07 03:39:49 +0900

    conftest 修正

--- unit_test_module/pytest_prac/tests/test_culc.py
+++ unit_test_module/pytest_prac/tests/test_culc.py
@@ -2,6 +2,8 @@ from .. import cluc
 import pytest
 
 is_release = True
+
+
 class TestCal:
     @classmethod
     def setup_class(cls):
@@ -11,7 +13,6 @@ class TestCal:
     def teardown_class(cls):
         print('end')
 
-    
     def setup_method(self, method):
         # テストそれぞれ開始時に呼ばれる
         print('setup')
@@ -30,7 +31,44 @@ class TestCal:
         with pytest.raises(ValueError):
             (self.cal.add_and_doubel("1", "1"))
 
-
-    @pytest.mark.skipif(is_release==True, reason="is_release")
+    @pytest.mark.skipif(is_release == True, reason="is_release")
     def test_add_num_and_double_skip_if(self):
         assert self.cal.add_and_doubel(1, 1) == 4
+
+'''
+# プラグイン使用方法
+def test_hello(pytester):
+    """Make sure that our plugin works."""
+
+    # create a temporary conftest.py file
+    pytester.makeconftest(
+        """
+        import pytest
+
+        @pytest.fixture(params=[
+            "Brianna",
+            "Andreas",
+            "Floris",
+        ])
+        def name(request):
+            return request.param
+    """
+    )
+
+    # create a temporary pytest test file
+    pytester.makepyfile(
+        """
+        def test_hello_default(hello):
+            assert hello() == "Hello World!"
+
+        def test_hello_name(hello, name):
+            assert hello(name) == "Hello {0}!".format(name)
+    """
+    )
+
+    # run all tests with pytest
+    result = pytester.runpytest()
+
+    # check that all 4 tests passed
+    result.assert_outcomes(passed=4)
+'''
